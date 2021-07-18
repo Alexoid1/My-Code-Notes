9c74503a7528bb00fdfea2551b19cc266f170c48
Alexoid1 <alex_zam@hotmail.es>
2021-07-17 21:57:24 -0500

    update readme file add article

--- src/addSnippet.js
+++ src/addSnippet.js
@@ -1,4 +1,4 @@
-const form= document.querySelector('form');
+const form= document.querySelector('#formS');
 const fs = require('fs');
 const path = require('path');
 const crypto = require('crypto');
@@ -13,10 +13,18 @@ let notes = [...student];
 form.addEventListener('submit', e => {
     e.preventDefault();
     let regex2=new RegExp('\n','gi');
+    let regex3=/(")/g;
+    let regex4=/(<)/g;
+    let regex5=/(>)/g;
     
     let title = document.getElementById('snippetTitle').value;
     let tech = document.getElementById('snippetTech').value;
-    let use = document.getElementById('snippetUse').value.replace(regex2,'<br>');
+    let use = document.getElementById('snippetUse').value.replace(regex2,'<br>')
+    console.log(use)
+    use.replace(regex3, '\"');
+    use.replace(regex4, '\<');
+    use.replace(regex5,'\>');
+    console.log(use)
     let description = document.getElementById('snippetDescription').value;
     let search = document.getElementById('snippetSearch').value;
     let formS=document.getElementById('formS');
